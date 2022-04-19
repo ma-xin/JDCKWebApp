6bccf1cfea3b36d29bd46f531bb1e5fbaad88a3a
maxinDev <527049190@qq.com>
2022-04-19 08:01:54 +0000

    青龙脚本兼容2.11.2 1. 青龙2.12.0以下（不包含）在 /ql/config/auth.json , 2.12.0（包含）以上在 /ql/data/config/auth.json 2. 青龙2.11.0 环境变量查询和修改接口字段 _id 改成 id 3. 青龙2.10.6以上版本，新增环境变量需要传数组

---
+++ temp/giteeCKConfig.py
@@ -0,0 +1,21 @@
+GiteeConfig = {
+    #gitEE Token令牌
+    'ACCESS_TOKE':'xxxxxxxxxxxxxxxx',
+    #gitEE owner
+    'OWNER':'xxxxxx',
+    #gitEE repo
+    'REPO' : 'xxxxxx',
+    #gitEE issue number
+    'NUMBER' : 'xxxxx'
+}
+
+QLConfig = {
+    #青龙登录账号
+    'QL_Login_name' : 'admin',
+    #青龙登录密码
+    'QL_Login_password' : 'admin',
+
+    # 127.0.0.1有的环境出现无法访问，最好还是改成本地ip
+    'URL_QL_domain' : 'http://192.168.0.155:5700/api/',
+    
+}
